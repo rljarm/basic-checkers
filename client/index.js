9d6f45959952214fe7c214bcb28e6a2fea4a2d6e
Dick <rljarm@gmail.com>
2015-04-12 17:07:56 -0700

    double jump fix

--- client/index.js
+++ client/index.js
@@ -1,238 +1,173 @@
 'use strict';
 
-var current = 'smith';
-
-var $selected;
-
-var enemy;
-
 $(document).ready(init);
 
-
+var current ='smith';
+var $source;
+var $target;
 
 function init(){
-
   initBoard();
-
-  switchBoard();
-
-
-
-  // only allow active clas to be clicked
-
-  $('#board').on('click', '.active', select);
-
-  $('#board').on('click', '.empty', drop);
-
+  switchUser();
+  $('table').on('click','.active',select);
+  $('table').on('click','.empty', move);
+  $('button').on('click',initBoard());
 }
 
-
-
-function select(){
-
-  $selected = $(this);
-
-  $('.valid').removeClass('selected');
-
-  $selected.addClass('selected');
-
-}
-
-
-
-function drop(){
-
-
-
-  if (!$selected) {
-
+function move(){
+  if(!$source){
     return;
-
   }
-
-
-
-  var $target = $(this);
-
-  // bool to check if selected is king
-
-  var isKing = $selected.is('.king');
-
-
+  $target = $(this);
+  var isKing = $source.is('.king');
 
   var src = {};
-
   var tgt = {};
 
-
-
-  src.x = $selected.data('x') * 1;
-
-  src.y = $selected.data('y') * 1;
-
+  src.x = $source.data('x') * 1;
+  src.y = $source.data('y') * 1;
   tgt.x = $target.data('x') * 1;
-
   tgt.y = $target.data('y') * 1;
 
-
-
-  // directional commands
-
   var compass = {};
-
-  compass.north = (current === 'js') ? -1 : 1;
-
-  compass.east = (current === 'js') ? 1 : 1;
-
+  compass.north = (current === 'smith') ? -1 : 1;
+  compass.east = (current === 'smith') ? 1 : -1;
   compass.west = compass.east * -1;
-
   compass.south = compass.north * -1;
 
-
-
-  switch(moveType(src, tgt, compass, isKing)){
-
+  switch(moveType(src,tgt,compass,isKing)){
     case 'move':
-
-      //console.log('move');
-
-      switchBoard();
-
-      movePiece($selected, $target);
-
+      movePiece($target,$source);
+      switchUser();
       break;
-
     case 'jump':
 
-      //console.log('jump');
+      movePiece($source, $target);
+      killMan(src,tgt,compass,isKing);
+        $source = $target;
 
-      switchBoard();
+        // dblJumpBool = true;
 
-      movePiece($selected, $target);
+        src.x = $source.data('x') * 1;
+        src.y = $source.data('y') * 1;
 
-  }
+        // code to check if double jump possible
+        $('td').each(function(e){
+          if ($(this).data('y') === src.y + (compass.north * 2) && ($(this).data('x') === src.x + (compass.east * 2) || $(this).data('x') === src.x + (compass.west * 2))){
+            $target = $(this)[0];
+
+            console.log($target);
+
+            if ($($target).hasClass('empty')){
+
+               var enemy = (current === 'chrome') ? 'chrome' : 'firefox';
+              $('.valid').removeClass('enemy');
+              $('.' + current).addClass('enemy');
+
+              tgt.x = $($target).data('x');
+              tgt.y = $($target).data('y');
+
+              var checkX = ((src.x + tgt.x) / 2);
+              var checkY = ((src.y + tgt.y) / 2);
+              var $middle = $('td[data-x=' + checkX + '][data-y='+ checkY +']');
+              $middle = $middle[0];
+              $middle.addClass('enemy');
+
+              if ($($middle).hasClass('enemy player')){
+                switchUser();
+              }
+
+            }
+          }
+        });
+
+        switchUser();
+      }
+    }
+
+
+function killMan(src,tgt,compass,isKing){
+  console.log('kill activated');
+  var $middle = inMiddle(src,tgt,compass,isKing);
+  $($middle).removeClass().addClass('valid empty');
 
 }
 
-
-
-function movePiece($selected, $target){
-
+function movePiece($target,$source){
   var targetClasses = $target.attr('class');
+  var sourceClasses = $source.attr('class');
 
-  var selectedClasses = $selected.attr('class');
+  $target.attr('class', sourceClasses);
+  $source.attr('class', targetClasses);
 
-
-
-  $target.attr('class', selectedClasses);
-
-  $selected.attr('class', targetClasses);
+  $target.data('y') === 0 ? $target.addClass('king smithKing') : console.log('shit monkey');
+  $target.data('y') === 7 ? $target.addClass('king neoKinged') : console.log('shit monkey');
 
 }
 
-
-
-function moveType(src, tgt, compass, isKing){
-
-
-
-  if (isMove(src, tgt, compass, isKing)){
-
-    return 'move';
-
-  }
-
-
-
-  if (isJump(src, tgt, compass, isKing) && isEnemy(src, tgt, compass, isKing)){
-
+function moveType(src,tgt,compass,isKing){
+  if (isJump(src,tgt,compass,isKing) && isEnemy(inMiddle(src,tgt,compass,isKing))){
     return 'jump';
-
   }
-
+  if(isMove(src,tgt,compass,isKing)){
+    return 'move';
+  }
 }
 
-
-
-function initBoard(){
-
-  $('#board tr:lt(3) .valid').addClass('neo player');
-
-  $('#board tr:gt(4) .valid').addClass('smith player');
-
-  $('td.valid:not(.player)').addClass('empty');
-
+function isMove(src,tgt,compass,isKing){
+  return (src.x + compass.east === tgt.x || src.x + compass.west === tgt.x) && (src.y + compass.north === tgt.y || (isKing && src.y + compass.south === tgt.y));
 }
-
-
-
-function isMove(src, tgt, compass, isKing){
-
-  // if tgt is left o right, north or south, is a king and  can go south
-
-  return (src.x + compass.east === tgt.x || src.x + compass.west === tgt.x) && (src.y + compass.north === tgt.y) || (src.y + compass.south === tgt.y) || (isKing && src.y + compass.south === tgt.y);
-
-}
-
-
 
 function isJump(src, tgt, compass, isKing){
 
-  // fix
+  var checkEast = compass.east * 2;
+  var checkWest = compass.west * 2;
+  var checkNorth = compass.north * 2;
+  var compassSouth = compass.south * 2;
 
-  return (src.x + (compass.east * 2) === tgt.x || src.x + (compass.west * 2) === tgt.x) && (src.y + (compass.north * 2) === tgt.y) || (src.y + (compass.south * 2) === tgt.y) || (isKing && src.y + (compass.south * 2) === tgt.y);
-
+  return (src.x + checkEast === tgt.x || src.x + checkWest === tgt.x) && (src.y + checkNorth === tgt.y) || (src.y + compassSouth === tgt.y) || (isKing && src.y + compassSouth === tgt.y);
 }
 
-
-
-function isEnemy(src, tgt, compass, isKing){
-
-  //console.log(src, tgt);
-  enemy = (current === 'neo') ? 'smith' : 'neo';
-  $('.valid').removeClass('enemy');
-  $('.' + current).addClass('enemy');
-
-
-
-
-  var checkX = (src.x + tgt.x) / 2;
-  checkX = checkX.toString();
-
-  var checkY = (src.y + tgt.y) / 2;
-    checkY = checkY.toString();
-    console.log(checkX, checkY);
-    //$('td[data-x='+checkX+'][data-y='+checkY+']')
-    var $middle = $('td[data-x='+checkX+'][data-y='+checkY+']');
-    $middle.removeClass().addClass('valid', 'empty');
-  //var middle = $('td').data('x', checkX).data('y', checkY);
-    //$('[data-x = ' + checkX + '] [data-y = ' + checkY + ']').removeClass().addClass('valid', 'empty');
-    //console.log($middle);
-  if ($middle.hasClass('enemy')){
-    //var enemy = !(current)
-    //$('.' + !current)
-    // middle.removeClass().addClass('valid', 'empty');
-    //middle.addClass('empty').removeClass('neo', 'smith', 'active', 'player');
-    $('[data-x = ' + checkX + '] [data-y = ' + checkY + ']').removeClass().addClass('valid', 'empty');
-    movePiece($selected, $target);
+function isEnemy($middle){
+  if ($($middle).hasClass('neo')&&($($source).hasClass('smith')) || ($($middle).hasClass('smith') && $($source).hasClass('neo'))){
     return true;
-
+  } else if ($($middle).hasClass('neoKinged')&&($($source).hasClass('smithKing')) || ($($middle).hasClass('smithKing') && $($source).hasClass('neoKinged'))){
+    return true;
+  } else if ($($middle).hasClass('neo')&&($($source).hasClass('smithKing')) || ($($middle).hasClass('smith') && $($source).hasClass('neoKinged'))){
+    return true;
+  } else if ($($middle).hasClass('neoKinged')&&($($source).hasClass('smith')) || ($($middle).hasClass('smithKing') && $($source).hasClass('neo'))){
+    return true;
+  } else {
+    return false;
   }
+}
 
-  return false;
+function inMiddle(src, tgt,compass, isKing){
+  var checkX = (src.x + tgt.x) / 2;
+  var checkY = (src.y + tgt.y) / 2;
+  var $middle = ($('td[data-x='+checkX+']td[data-y='+checkY+']'));
+  $middle = $middle[0];
+  return $middle;
+}
 
-  }
+function select(){
+  $source = $(this);
+  $('.valid').removeClass('selected');
+  $source.addClass('selected');
+}
 
+function isKing(){
+  return $source.hasClass('king');
+}
 
+function initBoard(){
+  $('tbody tr:lt(3) .valid').addClass('player neo');
+  $('tbody tr:gt(4) .valid').addClass('player smith');
+  $('tbody tr:lt(5):gt(2) .valid').addClass('empty');
+}
 
-
-
-function switchBoard(){
-
+function switchUser(){
   current = (current === 'neo') ? 'smith' : 'neo';
-
-  $('.valid').removeClass('active selected').addClass('inactive');
-
+  $('.valid').removeClass('active selected');
   $('.' + current).addClass('active');
-
 }
